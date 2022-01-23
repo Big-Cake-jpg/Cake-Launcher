816e1b96879f6e43f4c7e9fcad4d0b1bcd3f564a
XieXiLin158 <xiexilin158@qq.com>
2022-01-23 12:18:20 +0800

    Update:Step 5

--- Cake Launcher/MainWindow.xaml.cs
+++ Cake Launcher/MainWindow.xaml.cs
@@ -13,8 +13,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
-using KMCCC.Launcher;
-using KMCCC.Authentication;
 using System.Windows.Forms;
 using KMCCC.Tools;
 using Cake_Launcher.LoginUI;
@@ -31,20 +29,16 @@ namespace Cake_Launcher
         Offline Offline = new Offline();
         Mojang Mojang = new Mojang();
         LoginUI.Microsoft Microsoft = new LoginUI.Microsoft();
+        SquareMinecraftLauncher.Minecraft.Game game = new SquareMinecraftLauncher.Minecraft.Game();
         public int launchMode = 1;
         SquareMinecraftLauncher.Minecraft.Tools tools = new SquareMinecraftLauncher.Minecraft.Tools();
-        public static LauncherCore Core = LauncherCore.Create();
 
         public MainWindow()
         {
             InitializeComponent();
-            var versions = Core.GetVersions().ToArray();
+            var versions = tools.GetAllTheExistingVersion();
             versionCombo.ItemsSource = versions;
             List<string> javaList = new List<string>();
-            foreach(string i in KMCCC.Tools.SystemTools.FindJava())
-            {
-                javaList.Add(i);
-            }
             javaList.Add(tools.GetJavaPath());
             JavaPathCombo.ItemsSource = javaList;
             JavaPathCombo.SelectedItem = JavaPathCombo.Items[0];
@@ -52,19 +46,14 @@ namespace Cake_Launcher
         }
 
         public async void StartGame()
-        {
-            LaunchOptions launchOptions = new LaunchOptions();
             switch (launchMode)
             {
                 case 1:
-                    launchOptions.Authenticator = new OfflineAuthenticator(Offline.UserID.Text);
-                    break;
-                case 2:
-                    launchOptions.Authenticator = new YggdrasilLogin(Mojang.MojangEmail.Text, Mojang.MojangPassword.Password, false);
-                    break;
+                    await game.StartGame();
+                    break
             }
 
-            launchOptions.MaxMemory = Convert.ToInt32(MemoryTextBox.Text);
+            
             if (versionCombo.Text != string.Empty &&
                 JavaPathCombo.Text != string.Empty &&
                 (Offline.UserID.Text != string.Empty || (Mojang.MojangEmail.Text != string.Empty && Mojang.MojangPassword.Password != string.Empty) &&
@@ -73,37 +62,9 @@ namespace Cake_Launcher
 
                 try
                 {
-                    if (launchMode != 3)
-                    {
-                    Core.JavaPath = (string)JavaPathCombo.SelectedItem;
-                    var ver = (KMCCC.Launcher.Version)versionCombo.SelectedItem;
-                    launchOptions.Version = ver;
-
-                    var result = Core.Launch(launchOptions);
-                    if (!result.Success)
-                    {
-                        //MessageBox.Show(result.ErrorMessage, result.ErrorType.ToString());
-                        switch (result.ErrorType)
-                        {
-                            case ErrorType.NoJAVA:
-                                System.Windows.MessageBox.Show("你的 Java 环境存在问题，可能你曾删除过系统内的 Java，请尝试重新安装 Java\n详细信息：" + result.ErrorMessage, "错误", (MessageBoxButton)MessageBoxButtons.OK, (MessageBoxImage)MessageBoxIcon.Error);
-                                break;
-                            case ErrorType.AuthenticationFailed:
-                                System.Windows.MessageBox.Show(this, "正版验证失败！请检查你的账号密码", "账号错误\n详细信息：" + result.ErrorMessage, (MessageBoxButton)MessageBoxButtons.OK, (MessageBoxImage)MessageBoxIcon.Error);
-                                break;
-                            case ErrorType.UncompressingFailed:
-                                System.Windows.MessageBox.Show(this, "可能的多开或文件损坏，请确认文件完整且不要多开\n如果你不是多开游戏的话，请检查libraries文件夹是否完整\n详细信息：" + result.ErrorMessage, "可能的多开或文件损坏", (MessageBoxButton)MessageBoxButtons.OK, (MessageBoxImage)MessageBoxIcon.Error);
-                                break;
-                            default:
-                                System.Windows.MessageBox.Show(this,
-                                    result.ErrorMessage + "\n" +
-                                    (result.Exception == null ? string.Empty : result.Exception.StackTrace),
-                                    "启动错误，请将此窗口截图向开发者寻求帮助");
-                                break;
-                        }
-                    }
-                    }
-                    else
+                    
+                    
+                    
                     {
                         microsoft_launcher.MicrosoftAPIs microsoftAPIs = new microsoft_launcher.MicrosoftAPIs();
                         var v = Microsoft.browser.Source.ToString().Replace(microsoftAPIs.cutUri, string.Empty);
@@ -113,8 +74,8 @@ namespace Cake_Launcher
                         });
                         await t;
                         var v1 = microsoftAPIs.GetAllThings(t.Result.access_token, false);
-                        SquareMinecraftLauncher.Minecraft.Game game = new SquareMinecraftLauncher.Minecraft.Game();
-                        await game.StartGame(versionCombo.Text, JavaPathCombo.Text, Convert.ToInt32(MemoryTextBox.Text), v1.name, v1.uuid, v1.mcToken, string.Empty, string.Empty);
+                        
+                        
                     }
 
                 }
